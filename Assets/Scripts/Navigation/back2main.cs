5bb39bed2dec702ffcb5990452c65ec9f4b8c0f4
bobrocker20 <bond.davis@baruchmail.cuny.edu>
2014-08-31 19:11:53 -0400

    Menus

---
+++ Assets/Scripts/Navigation/back2main.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class back2main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnMouseDown () {
+			Application.LoadLevel("mainmenu");
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/Navigation/go2solo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class go2solo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnMouseDown () {
+		Application.LoadLevel ("solomenu");
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
