02cd81b84a3b4b09da0fe75cdd4305f395a7ab7e
JFFeeney <feeney.j.f@gmail.com>
2014-08-31 20:40:12 -0400

    Working sequences, reverse sequences are possible by uncommenting a line in nodebehavior onMouseOver.

--- Assets/Scripts/NodeBehavior.cs
+++ Assets/Scripts/NodeBehavior.cs
@@ -17,13 +17,12 @@ public class NodeBehavior : MonoBehaviour {
 		if (renderer.material.color != GameManager.newColor) {
 			GameManager.dragging = true;
 			renderer.material.color = GameManager.newColor;
-
 			GameManager.nodes.Add (gameObject);
 			GameManager.lastNode = gameObject;
 			print(gameObject.transform.position);
-
-
 			GameManager.nodes.Add(gameObject);
+
+
 
 		}
 	}
@@ -31,25 +30,30 @@ public class NodeBehavior : MonoBehaviour {
 	// successive nodes
 	void OnMouseOver() {
 		if (GameManager.dragging && GameManager.nodes.Count > 0 && renderer.material.color != GameManager.newColor) {
-
-			//print(GameManager.nodes.Count);
-
-//			print (gameObject.transform.position);
-//			print (GameManager.lastNode.transform.position);
-//			print (Vector3.Distance(gameObject.transform.position, GameManager.lastNode.transform.position));
 			if( Vector3.Distance(gameObject.transform.position, GameManager.lastNode.transform.position) <= 1.5) {
-				renderer.material.color = GameManager.newColor;
-				GameManager.nodes.Add(gameObject);
-				GameManager.lastNode = gameObject;
+				int lastNodeTexture = int.Parse(GameManager.lastNode.tag);
+				int currNodeTexture = int.Parse(gameObject.renderer.tag);
+
+				if(GameManager.direction == 0) {
+					if(lastNodeTexture - currNodeTexture == 1)
+					{
+						//Allows for reverse sequences i.e 6,5,4,3,2,1.
+						//GameManager.direction = -1;
+					}
+					else if(lastNodeTexture - currNodeTexture == -1)
+					{
+						GameManager.direction = 1;
+					}
+				}
+
+				if(GameManager.direction != 0) {
+					if( lastNodeTexture - currNodeTexture + GameManager.direction == 0) {
+						renderer.material.color = GameManager.newColor;
+						GameManager.nodes.Add(gameObject);
+						GameManager.lastNode = gameObject;
+					}
+				}
 			}
-//			foreach (GameObject node in GameManager.nodes) {
-//				print(node.renderer.material.mainTexture);
-//			}
-
-
-			//if( Vector3.Distance(gameObject.transform.position, GameManager.lastNode.transform.position) <= 2^.5)
-			//print(gameObject.transform.position);
-
 		}
 	}
 
@@ -67,5 +71,6 @@ public class NodeBehavior : MonoBehaviour {
 			}
 		}
 		GameManager.countTemp = 0;
+		GameManager.direction = 0;
 	}
 }
