56255f1355f08316273aae104b4fb5a892c3a0bd
John Lee <leedhyun@umich.edu>
2014-09-02 23:53:10 -0400

    Improved radial progress clock

--- Assets/Scripts/ClockBehavior.cs
+++ Assets/Scripts/ClockBehavior.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class ClockBehavior : MonoBehaviour {
 
-	private int timeToComplete = 60;
-	
+	private int timeToComplete = 10;
+	private GameObject clockMask, clockEnd;
+
 	// Use this for initialization
 	void Start () {
-		//Use this to Start progress
+		clockMask = GameObject.Find("ClockMask");
+		clockEnd = GameObject.Find("ClockEnd");
+
 		StartCoroutine(ClockUpdate(timeToComplete));
 	}
 	
@@ -18,10 +21,17 @@ public class ClockBehavior : MonoBehaviour {
 		while (fractionComplete < 1)
 		{
 			fractionComplete += rate * Time.deltaTime;
-			gameObject.renderer.material.SetFloat("_Cutoff", fractionComplete);
 
-			if(fractionComplete >= 1) {
+			if(fractionComplete > 1) {
 				Application.LoadLevel("gameoverscreen");
+			}
+
+			if(fractionComplete < 0.5f) {
+				clockMask.transform.rotation = Quaternion.Euler(0, 0, -360*fractionComplete);
+			}
+			else {
+				clockMask.SetActive(false);
+				clockEnd.transform.rotation = Quaternion.Euler(0, 0, -360*(fractionComplete-0.5f));
 			}
 
 			yield return 0;
---
+++ Assets/Shaders/InvisibleMask.shader
@@ -0,0 +1,9 @@
+﻿Shader "Custom/InvisibleMask" {
+	SubShader {
+		// render after all opaque and transparent objects
+		Tags { "Queue"="Transparent+1" }
+		
+		// keep the image behind it
+		Pass { Blend Zero One }
+	} 
+}
