d2060b91a228c73431662007f15b576716ecf3e9
Joe Lee <perchl0rat1c@gmail.com>
2014-09-09 00:22:55 -0400

    changed default node textures

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 	// Static, hide from Inspector
 	public static Color newColor = Color.blue;
-	public static Color oldColor = Color.green;
+	public static Color oldColor = Color.white;
 	public static ArrayList nodes = new ArrayList();
 	public static bool dragging;
 	public static GameObject lastNode;
@@ -33,7 +33,7 @@ public class GameManager : MonoBehaviour {
 		Vector3 offset = new Vector3(-cols/2.0f+1.0f, -rows/2.0f+1.0f, 0.0f);
 		for (int i=0; i<rows; i++) {
 			for (int j=0; j<cols; j++) {
-				spawnGrid[j,i] = Instantiate(node, new Vector3(j,i,0.0f)+offset, Quaternion.Euler(0,0,180)) as GameObject;
+				spawnGrid[j,i] = Instantiate(node, new Vector3(j,i,0.0f)+offset, Quaternion.Euler(0,120,0)) as GameObject;
 				print("j="+j+", i="+i+spawnGrid[j,i]);
 			}
 		}
